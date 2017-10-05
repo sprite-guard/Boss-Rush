c3de5526c0fd6036c9896cb2650ecaa0b838fafd
Sprite Guard <spriteguard@perpetual-dawn.com>
2017-10-05 03:05:16 -0700

    Make the dash target look fancy
    Dash target now has two concentric rings that rotate in opposite
    directions, and a dot at the center that is always visible.

--- js/dashtarget.js
+++ js/dashtarget.js
@@ -2,6 +2,8 @@ function DashTarget() {
   this.x_offset = 0;
   this.y_offset = 0;
   this.r = 16;
+  this.inner_r = 12;
+  this.core_r = 5;
   this.dx = 0;
   this.dy = 0;
   this.speed = 3;
@@ -14,6 +16,14 @@ function DashTarget() {
       this.x_offset += this.dx;
       this.y_offset += this.dy;
       
+      this.x = player.x + this.x_offset;
+      this.y = player.y + this.y_offset;
+      
+      if(this.x < 0) this.x = 0;
+      if(this.x > game.canvas.width) this.x = game.canvas.width;
+      if(this.y < 0) this.y = 0;
+      if(this.y > game.canvas.height) this.y = game.canvas.height;
+      
       this.draw();
       player.dash_fuel--;
     } else {
@@ -25,13 +35,18 @@ function DashTarget() {
   this.draw = function() {
     var angle_to_draw = ((player.dash_fuel / player.max_dash_fuel) * 2 * Math.PI);
     
-    var x = player.x + this.x_offset;
-    var y = player.y + this.y_offset;
     
     game.draw.beginPath();
     game.draw.lineWidth = 3;
     game.draw.strokeStyle = "#FFFFFF";
-    game.draw.ellipse(x, y, this.r, this.r, 0, 0, angle_to_draw);
+    game.draw.ellipse(this.x, this.y, this.r, this.r, 0, 0, angle_to_draw);
     game.draw.stroke();
+    game.draw.beginPath();
+    game.draw.ellipse(this.x, this.y, this.inner_r, this.inner_r, 0, 0, (2 * Math.PI) - angle_to_draw, true);
+    game.draw.stroke();
+    game.draw.beginPath();
+    game.draw.fillStyle = "#999900";
+    game.draw.ellipse(this.x, this.y, this.core_r, this.core_r, 0, 0, (2 * Math.PI));
+    game.draw.fill();
   }
 }
