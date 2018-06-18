641233203622458dfab5683df5fb41b1d3997f45
Sprite Guard <parker.tristan.james@gmail.com>
2018-06-17 21:04:20 -0700

    More attacks for Dream of a Child

--- js/boss/hope.js
+++ js/boss/hope.js
@@ -344,6 +344,230 @@ var hope = {
                 max_age: 1000
               }
             },
+            {
+              sources: helpers.makeRingAttack(400,300,9,0,1),
+              x: 0,
+              y: 0,
+              spin: 0.125*Math.PI,
+              random_spread: 0,
+              delay: 40,
+              sync: 1440,
+              lifespan: 500,
+              speed_jitter: 0,
+              immediate: true,
+              bullet_type: {
+                behaviors: [ BulletBehavior.explode ],
+                parameters: {
+                  explode_time: 40,
+                  explode_spawner: {
+                    sources: helpers.makeRingAttack(400,300,5,0,1),
+                    x: 0,
+                    y: 0,
+                    spin: 0,
+                    random_spread: 0,
+                    delay: 1,
+                    sync: 0,
+                    lifespan: 1,
+                    speed_jitter: 0,
+                    yaw_jitter: 0.002,
+                    immediate: true,
+                    behaviors: [ SpawnerBehavior.freeze, SpawnerBehavior.scatter ],
+                    parameters: {
+                      freeze_countdown: 40,
+                      freeze_duration: 100,
+                      scatter_countdown: (40+100),
+                      scatter_target: {x: 400, y: 300},
+                      scatter_beam_width: Math.PI/4,
+                      scatter_direction: 1
+                    },
+                    bullet_type: {
+                      behaviors: [  ],
+                      parameters: {
+                      },
+                      yaw: 0,
+                      speed: 3,
+                      r: 8,
+                      color: "#FF88FF",
+                      shell: "#FF44FF",
+                      graze_color: "#991199",
+                      style: "gradient",
+                      cull_type: "screen",
+                      max_age: 1000
+                    }
+                  }
+                },
+                yaw: 0,
+                speed: 3,
+                r: 8,
+                color: "#FF88FF",
+                shell: "#FF44FF",
+                graze_color: "#991199",
+                style: "gradient",
+                cull_type: "screen",
+                max_age: 1000
+              }
+            },
+            {
+              sources: helpers.makeRingAttack(400,300,6,0,1),
+              x: 400, y: 300, heading: 0,
+              spin: 0.07,
+              delay: 3,
+              sync: 1920,
+              lifespan: 220,
+              random_spread: 0,
+              speed_jitter: 0,
+              yaw_jitter: 0,
+              bullet_type: {
+                yaw: 0,
+                speed: 3,
+                r: 8,
+                color: "#99AACC",
+                shell: "#338866",
+                graze_color: "#000000",
+                style: "gradient",
+                cull_type: "screen",
+                max_age: 1000,
+              }
+            },
+            {
+              sources: helpers.makeRingAttack(400,300,6,0,1),
+              x: 400, y: 300, heading: 0,
+              spin: -0.07,
+              delay: 3,
+              sync: 1920,
+              lifespan: 220,
+              random_spread: 0,
+              speed_jitter: 0,
+              yaw_jitter: 0,
+              bullet_type: {
+                yaw: 0,
+                speed: 3,
+                r: 8,
+                color: "#99AACC",
+                shell: "#338866",
+                graze_color: "#000000",
+                style: "gradient",
+                cull_type: "screen",
+                max_age: 1000,
+              }
+            },
+            {
+              sources: helpers.makeRingAttack(400,300,6,0,1),
+              x: 400, y: 300, heading: 0,
+              spin: 0.5,
+              delay: 3,
+              sync: 2180,
+              lifespan: 220,
+              random_spread: 0,
+              speed_jitter: 0,
+              yaw_jitter: 0,
+              bullet_type: {
+                yaw: 0.01,
+                speed: 3,
+                r: 8,
+                color: "#99AACC",
+                shell: "#338866",
+                graze_color: "#000000",
+                style: "gradient",
+                cull_type: "screen",
+                max_age: 1000,
+              }
+            },
+            {
+              sources: helpers.makeRingAttack(400,300,6,0,1),
+              x: 400, y: 300, heading: 0,
+              spin: -0.5,
+              delay: 3,
+              sync: 2180,
+              lifespan: 220,
+              random_spread: 0,
+              speed_jitter: 0,
+              yaw_jitter: 0,
+              bullet_type: {
+                yaw: -0.01,
+                speed: 3,
+                r: 8,
+                color: "#99AACC",
+                shell: "#338866",
+                graze_color: "#000000",
+                style: "gradient",
+                cull_type: "screen",
+                max_age: 1000,
+              }
+            },
+            {
+              sources: helpers.makeRingAttack(400,300,24,0,1),
+              x: 0, y: 0, heading: 0, spin: 0, random_spread: 0,
+              delay: 40,
+              sync: 2440,
+              lifespan: 220,
+              aimed: true,
+              bullet_type: {
+                behaviors: [ BulletBehavior.bounce ],
+                parameters: {
+                  bounces: 3
+                },
+                yaw: 0,
+                speed: 3,
+                r: 8,
+                color: "#FF5522",
+                shell: "#FF5522",
+                graze_color: "#FF0000",
+                style: "solid",
+                cull_type: "screen",
+                max_age: 1000
+              }
+            },
+            {
+              sources: helpers.makeRingAttack(400,300,24,0,1),
+              x: 0, y: 0, heading: 0, spin: 0, random_spread: 0,
+              delay: 40,
+              sync: 3500,
+              lifespan: 220,
+              aimed: true,
+              bullet_type: {
+                behaviors: [ BulletBehavior.shootback ],
+                parameters: {
+                  shootback_spawner: {
+                    sources: [{
+                      x: 0,
+                      y: 0,
+                      heading: 0
+                    }],
+                    x: 0, y: 0, heading: 0, spin: 0, random_spread: 0,
+                    delay: 40,
+                    sync: 3500,
+                    lifespan: 220,
+                    behaviors: [],
+                    parameters: {},
+                    aimed: true,
+                    bullet_type: {
+                      behaviors: [  ],
+                      parameters: {
+                      },
+                      yaw: 0,
+                      speed: 3,
+                      r: 8,
+                      color: "#0066FF",
+                      shell: "#0044CC",
+                      graze_color: "#000000",
+                      style: "gradient",
+                      cull_type: "screen",
+                      max_age: 1000
+                    }
+                  }
+                },
+                yaw: 0,
+                speed: 3,
+                r: 8,
+                color: "#0066FF",
+                shell: "#0044CC",
+                graze_color: "#000000",
+                style: "gradient",
+                cull_type: "screen",
+                max_age: 1000
+              }
+            }
             
           ],
           choreography: {}
